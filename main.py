b3d21a14457e738c2438b5b3a1b7ccc69f7c33cb
MaksimAliev <imaxaliev@gmail.com>
2026-05-03 17:33:54 +0300

    add entry point

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.modelfast.domain import Dataset
+from src.modelfast.api import Modeler
+from src.modelfast.repository import ImbalancedDatasetRepository
+
+def main():
+    datasets = ImbalancedDatasetRepository(verbosity=2).load_datasets()
+
+    modelfind = Modeler(
+        backend='modelfast',
+        metric='f1',
+        extra_metrics=['precision', 'recall'],
+        timeout=3600,
+        verbosity=2
+    )
+
+    for dataset in datasets:
+        modelfind.run(dataset)
+
+
+if __name__ == '__main__':
+    main()
